5e67d8fba1c4778ed77c1c63ea0048dc59ab6bc0
Daniel Falbel <dfalbel@gmail.com>
2016-04-27 12:55:05 -0400

    start

---
+++ R/stem.R
@@ -0,0 +1,10 @@
+
+
+palavras <- c("casamento", "casado", "casada", "casadas", "casório")
+
+hunspell::hunspell_analyze(palavras, dict = "inst/Portuguese (Brazilian).dic")
+
+
+help(hunspell_analyze)
+
+help(distinct)
